74b8abb78ebb721675b45ba5628e611379d71589
Aniket <aniketjadhav310@gmail.com>
2024-09-03 13:40:37 +0530

    complted

--- company/api/admin.py
+++ company/api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from api.models import Company,Employee
 # Register your models here.
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display=('name','location','type')
+    search_fields=('name',)
+
+class EmpAdmin(admin.ModelAdmin):
+    list_display=('name','address','phone')
+    list_filter=('company',)
+    
+
+
+admin.site.register(Company,CompanyAdmin)
+admin.site.register(Employee,EmpAdmin)
--- company/api/models.py
+++ company/api/models.py
@@ -11,3 +11,18 @@ class Company(models.Model):
     type=models.CharField(max_length=100,choices=Company_types)
     added_date=models.DateTimeField(auto_now=True)
     active=models.BooleanField(default=True)
+
+    def __str__(self) :
+        return self.name
+
+class Employee(models.Model):
+    emp_id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=100)
+    email=models.CharField(max_length=50)
+    address=models.CharField(max_length=100)
+    phone=models.CharField(max_length=15)
+    position=models.CharField(max_length=50,choices=[('MN','Manage'),('SD','Software Developer'),('HR','HR')])
+    company=models.ForeignKey(Company,on_delete=models.CASCADE)  
+
+    def __str__(self):
+        return self.name
